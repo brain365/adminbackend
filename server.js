6c3d12a05a41f5f0a348ae163f2d1b727caaca32
mishan <rajendra.brainbinary@gmail.com>
2023-12-22 11:31:45 +0530

    cors final

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cookieParser = require('cookie-parser');
 dbConnect();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'https://adminfrontend-five.vercel.app/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
